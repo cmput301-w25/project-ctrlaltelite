044017cf19c3e9d25370209ed8e734f3fff65461
Raiyana Rahman <raiyana2@ualberta.ca>
2025-02-28 02:05:32 -0700

    Added the splash screen, fixed the background to white no matter which mode

--- code/app/src/main/java/com/example/ctrlaltelite/SplashActivity.java
+++ code/app/src/main/java/com/example/ctrlaltelite/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.ctrlaltelite;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,26 +10,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-public class MainActivity extends AppCompatActivity {
-    private FirebaseFirestore db;
-
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        db = FirebaseFirestore.getInstance();
-
-
+        setContentView(R.layout.activity_splash);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+
+                }
+                finally {
+                    Intent intent = new Intent(SplashActivity.this , MainActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        };thread.start();
     }
 }
